11f1c72483c2f59eb31970457721684f134922e8
iwakitakuma33 <iwakitakuma33@gmail.com>
2022-03-20 22:15:40 +0900

    [更新]open api config情報を更新しました。

---
+++ src/constants/api.constants.ts
@@ -0,0 +1,5 @@
+export const API_BASE_URL = 'https://api.openweathermap.org/data/2.5';
+export const ICON_BASE_URL = 'https://openweathermap.org/img/wn';
+
+export const API_WEATHER_URL = `${API_BASE_URL}/weather`;
+export const API_ONECALL_URL = `${API_BASE_URL}/onecall`;
