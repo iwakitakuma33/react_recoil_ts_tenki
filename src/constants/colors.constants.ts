086fd144b34cc67072d3c10f805d6a410594dadf
iwakitakuma33 <iwakitakuma33@gmail.com>
2022-03-21 18:12:34 +0900

    [追加]定数ファイル追加 カラー情報|天気グラフconfig

---
+++ src/constants/colors.constants.ts
@@ -0,0 +1,3 @@
+export const COLOR_DARK_GREY = '#48484A';
+export const COLOR_DARK_BLUE = '#6495ed';
+export const COLOR_PALE_RED = '#DD7555';
---
+++ src/constants/defaults.constants.ts
@@ -0,0 +1,7 @@
+import { Units } from './units.constants';
+
+export const DEFAULT_CITY = 'Tokyo';
+export const DEFAULT_UNITS = Units.Metric;
+
+export const HOURLY_FORECAST_DEFAULT_LENGTH = 25;
+export const DAILY_FORECAST_DEFAULT_LENGTH = 8;
